975734ea4bd7c3c00836dfe919ffedf892a7d7bb
DESKTOP-K85TRKG\diogo <diogoassuncao@msn.com>
2021-10-28 16:48:58 +0200

    Crear servidor express y cron

--- index.js
+++ index.js
@@ -1,115 +1,128 @@
 const config = require('./package.json');
 //Importan la libreria -> npm i puppeteer
+const cron = require('node-cron');
+const express = require('express');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-console.time('tiempo ejecución');
+//Creamos el servidor express
+const app = express();
+//Hacemos que el servidor escuche el puerto 3000
+app.listen('3000', function() {
+  console.log('Servidor web escuchando en el puerto 3000');
+});
 
-(async () => {
-    //Cargamos el navegador
-    const browser = await puppeteer.launch();
-    //Llamamos al navegador para que abra una pagina en blanco
-    const page = await browser.newPage();
-    //Nos dirimos a la web que nos interesa
-    await page.goto(config.url_web);
+//Creamos el cron web
+cron.schedule('30 * * * * *', function() {
+    console.time('tiempo ejecución');
 
-    //Obtenemos los links de los diferentes Mangas
-    const links = await page.evaluate(() => {
-        let links = [];
-        let elements = document.querySelectorAll("table.ventana_id1 td.izq a");
+    (async () => {
+        //Cargamos el navegador
+        const browser = await puppeteer.launch();
+        //Llamamos al navegador para que abra una pagina en blanco
+        const page = await browser.newPage();
+        //Nos dirimos a la web que nos interesa
+        await page.goto(config.url_web);
 
-        for(let element of elements) {
-            links.push(element.href);
-        }
-        
-        return links;
-    })
+        //Obtenemos los links de los diferentes Mangas
+        const links = await page.evaluate(() => {
+            let links = [];
+            let elements = document.querySelectorAll("table.ventana_id1 td.izq a");
 
-    const listFullMangas = []
-    for(let link of links){
-        await page.goto(link);
-        //await page.waitForTimeout(1000);
-
-        let element = await page.evaluate(() => {
-            //Definir el bloque de los datos generales    
-            let element = document.querySelector("table.ventana_id1 td.izq").childNodes;
-
-            let indexJapon = -1;
-            let indexEspanol = -1;
-            for(let [index, tags] of element.entries()){
-                if(tags.innerText == "Números en japonés:"){
-                    indexJapon = index + 1;
-                }
-
-                if(tags.innerText == "Números en español:"){
-                    indexEspanol = index + 1;
-                }
-            }
-
-            //Crear objecto JSON con los dato basicos
-            let estadoJapon = null;
-            if(indexJapon != -1){
-                estadoJapon = element[indexJapon].textContent.split('(')[1].slice(0, -1);
-            }
-               
-            let estadoEspanol = null;
-            if(indexEspanol != -1){
-                estadoEspanol = element[indexEspanol].textContent.split('(')[1].slice(0, -1);
-            }    
-                
-
-            let objJSON = {};
-            objJSON.title = element[1].innerText;
-
-            if(indexJapon != -1){
-                objJSON.numJapon = element[indexJapon].textContent.split('(')[0].trim();
-                objJSON.estadoJapon = estadoJapon;
+            for(let element of elements) {
+                links.push(element.href);
             }
             
-            if(indexEspanol != -1){
-                objJSON.numEspanol = element[indexEspanol].textContent.split('(')[0].trim();
-                objJSON.estadoEspanol = estadoEspanol;
-            }
-            
-            
-            let listMangas = []
-            let mangas = document.querySelectorAll("table.ventana_id1 td.cen a");
-            for(let [index, manga] of mangas.entries()){
-                if((index + 1) <= Number(objJSON.numEspanol)){
-                    if(!isNaN(Date.parse(manga.innerText))){
-                        let date = manga.innerText
-
-                        let padreTabla = manga.parentElement.childNodes
-                        if(Number.isInteger(parseInt(padreTabla[padreTabla.length - 2].textContent.trim()))){
-                            date = padreTabla[padreTabla.length - 2].textContent.concat(date);       
-                        }
-
-                        listMangas.push({"numTomo": (index + 1), "fecha": date})
-                    }
-                }
-            }
-
-            //
-            objJSON.listMangas = listMangas;
-
-            //return objJSON;
-            return objJSON;
+            return links;
         })
 
-        element.id = link.split('=')[1];
+        const listFullMangas = []
+        for(let link of links){
+            await page.goto(link);
+            //await page.waitForTimeout(1000);
 
-        listFullMangas.push(element);
-        console.log("Se ha guardado el manga " + element.id);
-    }
+            let element = await page.evaluate(() => {
+                //Definir el bloque de los datos generales    
+                let element = document.querySelector("table.ventana_id1 td.izq").childNodes;
 
-    //IMPORTANTE cerrar el navegador
-    await browser.close();
+                let indexJapon = -1;
+                let indexEspanol = -1;
+                for(let [index, tags] of element.entries()){
+                    if(tags.innerText == "Números en japonés:"){
+                        indexJapon = index + 1;
+                    }
 
-    let jsonFile = {"listMangas": listFullMangas}
-    fs.writeFile(config.uri_json, JSON.stringify(jsonFile),'utf8', (err) => { 
-        if (err) throw err; 
-            console.log('The file has been saved!'); 
-    }); 
+                    if(tags.innerText == "Números en español:"){
+                        indexEspanol = index + 1;
+                    }
+                }
 
-    console.timeEnd('tiempo ejecución');
-})();
+                //Crear objecto JSON con los dato basicos
+                let estadoJapon = null;
+                if(indexJapon != -1){
+                    estadoJapon = element[indexJapon].textContent.split('(')[1].slice(0, -1);
+                }
+                
+                let estadoEspanol = null;
+                if(indexEspanol != -1){
+                    estadoEspanol = element[indexEspanol].textContent.split('(')[1].slice(0, -1);
+                }    
+                    
+
+                let objJSON = {};
+                objJSON.title = element[1].innerText;
+
+                if(indexJapon != -1){
+                    objJSON.numJapon = element[indexJapon].textContent.split('(')[0].trim();
+                    objJSON.estadoJapon = estadoJapon;
+                }
+                
+                if(indexEspanol != -1){
+                    objJSON.numEspanol = element[indexEspanol].textContent.split('(')[0].trim();
+                    objJSON.estadoEspanol = estadoEspanol;
+                }
+                
+                
+                let listMangas = []
+                let mangas = document.querySelectorAll("table.ventana_id1 td.cen a");
+                for(let [index, manga] of mangas.entries()){
+                    if((index + 1) <= Number(objJSON.numEspanol)){
+                        if(!isNaN(Date.parse(manga.innerText))){
+                            let date = manga.innerText
+
+                            let padreTabla = manga.parentElement.childNodes
+                            if(Number.isInteger(parseInt(padreTabla[padreTabla.length - 2].textContent.trim()))){
+                                date = padreTabla[padreTabla.length - 2].textContent.concat(date);       
+                            }
+
+                            listMangas.push({"numTomo": (index + 1), "fecha": date})
+                        }
+                    }
+                }
+
+                //
+                objJSON.listMangas = listMangas;
+
+                //return objJSON;
+                return objJSON;
+            })
+
+            element.id = link.split('=')[1];
+
+            listFullMangas.push(element);
+            console.log("Se ha guardado el manga " + element.id);
+        }
+
+        //IMPORTANTE cerrar el navegador
+        await browser.close();
+
+        let jsonFile = {"listMangas": listFullMangas}
+        fs.writeFile(config.uri_json, JSON.stringify(jsonFile),'utf8', (err) => { 
+            if (err) throw err; 
+                console.log('The file has been saved!'); 
+        }); 
+
+        console.timeEnd('tiempo ejecución');
+
+    })();
+});
