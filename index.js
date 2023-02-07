60cb1adc7abcfb175558e909c7b9546ff43c44f6
Thekiso <diogoassuncao@msn.com>
2023-02-07 18:21:29 +0100

    Hacer que el servidor Express escuche peticiones a endpoint. Elimiar petición al endpoint '/'.

--- index.js
+++ index.js
@@ -12,26 +12,28 @@ const scraping = require('./functions/scrapingWeb');
 //Creamos el servidor express
 const app = express();
 const host = 'localhost';
-const port = 3000;
+const port = 8000;
 
 //Hacemos que el servidor escuche el puerto 3000
 app.listen(port, function() {
-  console.log(`Servidor web escuchando en el puerto ${port}`);
+  console.log(`Servidor web escuchando en el puerto http://${host}:${port}`);
 });
 
-const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
+app.get("/api/getJsonScraping", (req, res) => {
+    console.time('tiempo ejecución');
+    res.header("Access-Control-Allow-Origin"); //Evitar problemas con los CORPS
 
-    scraping.ExecuteScraping(puppeteer, config, fs);
 
-    res.end(`{"message": "Test de la función ejecutado. Revisa los logs"}`);
-};
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+    res.json({status: 200, message: "Ok"});
+    console.timeEnd('tiempo ejecución');
+})
+
+app.get('/', (req, res) => {
+    console.log("Forwarded header =>", req.headers['X-Forwarded-For']);
+    res.header("Access-Control-Allow-Origin"); //Evitar problemas con los CORPS
+    res.json({status: 200, message: `Server is running on http://${host}:${port}`});
+})
 
 //Creamos el cron web
 cron.schedule('0 3 * * *', function() {
