b651442bbca0cb78119a32bc54d7e1e53a527b4e
Thekiso <diogoassuncao@msn.com>
2023-09-07 15:24:05 +0200

    Empezar la reconstrución del scraping web a la versión 2.0.0

---
+++ src/test/java/com/thekiso/dragoreign/projectblue/ProjectBlueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thekiso.dragoreign.projectblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
